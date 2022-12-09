c073ee4221a8084cfc7016627b010f7858f656c8
marsen <marsenzakirov11@gmail.com>
2022-12-09 13:32:05 +0300

    fix: add each module to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { RoleModule } from './user/role/role.module';
 import { FileModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
 import { Module } from '@nestjs/common';
@@ -5,16 +6,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 ConfigModule.forRoot();
 @Module({
   imports: [
-    TrackModule,
     MongooseModule.forRoot(process.env.DB_URL),
+    TrackModule,
     FileModule,
+    UserModule,
+    AuthModule,
+    RoleModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, 'static'),
     }),
   ],
+  providers: [],
 })
 export class AppModule {}
